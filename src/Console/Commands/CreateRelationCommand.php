f6967bde50846e6aaccd26aef3a474a0beb07751
Oleg Pashkovsky <oleg@pashkovsky.me>
2024-04-13 11:52:14 +0300

    Update code

--- src/Console/Commands/CreateRelationCommand.php
+++ src/Console/Commands/CreateRelationCommand.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 
 class CreateRelationCommand extends Command
 {
@@ -20,90 +20,60 @@ class CreateRelationCommand extends Command
     public function handle()
     {
         $type = $this->argument('type');
-        $firstModel = $this->argument('firstModel');
-        $secondModel = $this->argument('secondModel');
+        $firstModel = ucfirst($this->argument('firstModel'));
+        $secondModel = ucfirst($this->argument('secondModel'));
         $reverse = $this->option('reverse');
-        $namespace = $this->option('namespace') ?? config('eloqrait.namespace', 'App\Models\Traits');;
+        $namespace = $this->option('namespace') ?? config('eloqrait.namespace', 'App\Models\Traits');
         $modelNamespace = $this->option('model-namespace') ?? config('eloqrait.model_namespace', 'App\Models');
 
-        $stub = File::get(__DIR__ . '/../stubs/' . $type . '.stub');
-        $modelName1 = ucfirst($firstModel);
-        $modelName2 = ucfirst($secondModel);
-        $modelName1Plural = str_plural($modelName1);
-        $modelName2Plural = str_plural($modelName2);
-        $relationName = lcfirst($modelName2Plural);
-        $reverseRelationName = lcfirst($modelName1);
-        $reverseModelName = ucfirst($firstModel);
-
-        $stub = str_replace(
-            ['{{ modelName1 }}', '{{ modelName2 }}', '{{ modelName1Plural }}', '{{ modelName2Plural }}', '{{ relationName }}', '{{ reverseRelationName }}', '{{ reverseModelName }}', '{{ namespace }}', '{{ modelNamespace }}'],
-            [$modelName1, $modelName2, $modelName1Plural, $modelName2Plural, $relationName, $reverseRelationName, $reverseModelName, $namespace, $modelNamespace],
-            $stub
-        );
-
-        $filePath1 = app_path('Models/' . $modelName1 . '.php');
-        $filePath2 = app_path('Models/' . $modelName2 . '.php');
-
-        $this->createFileIfNotExists($filePath1);
-        $this->createFileIfNotExists($filePath2);
-
-        File::append($filePath1, "\n\n" . $stub);
-        if (!$reverse) {
-            File::append($filePath2, "\n\n" . $stub);
-        } else {
-            $reverseStub = File::get(__DIR__ . '/stubs/belongsTo.stub');
-            $reverseStub = str_replace(
-                ['{{ modelName1 }}', '{{ modelName2 }}', '{{ modelName1Plural }}', '{{ modelName2Plural }}', '{{ relationName }}', '{{ reverseRelationName }}', '{{ reverseModelName }}', '{{ namespace }}', '{{ modelNamespace }}'],
-                [$modelName2, $modelName1, $modelName2Plural, $modelName1Plural, $reverseRelationName, $relationName, $reverseModelName, $namespace, $modelNamespace],
-                $reverseStub
-            );
-            File::append($filePath2, "\n\n" . $reverseStub);
-        }
-
+        $this->generateRelations($type, $firstModel, $secondModel, $namespace, $modelNamespace, $reverse);
         $this->info('Relation created successfully.');
     }
 
-    private function createFileIfNotExists($filePath)
+    private function generateRelations($type, $firstModel, $secondModel, $namespace, $modelNamespace, $reverse)
     {
-        if (File::exists($filePath)) {
-            return;
+        $relationStub = File::get($this->getStubPath($type));
+        $this->createModelFile($firstModel, $modelNamespace);
+        $this->createModelFile($secondModel, $modelNamespace);
+        $this->appendRelation($type, $firstModel, $secondModel, $modelNamespace, $relationStub);
+
+        if ($reverse) {
+            $reverseStub = File::get($this->getStubPath('belongsTo'));
+            $this->appendRelation('belongsTo', $secondModel, $firstModel, $modelNamespace, $reverseStub);
         }
-
-        $modelNamespace = $this->option('model-namespace') ?? config('eloqrait.model_namespace', 'App\Models');
-        $modelName = basename($filePath, '.php');
-        $content = "<?php\n\nnamespace {$modelNamespace};\n\nuse Illuminate\Database\Eloquent\Model;\n\nclass {$modelName} extends Model\n{\n    //\n}\n";
-
-        File::put($filePath, $content);
-
-        $this->info("Model created at: {$filePath}");
     }
 
-    private function createTrait($type, $firstModel, $secondModel, $reverse)
+    private function getStubPath($type)
     {
-        $traitNamespace = $this->argument('namespace') ?? config('eloqrait.namespace', 'App\Traits');
-        $modelNamespace = $this->option('model-namespace') ?? config('eloqrait.models_namespace', 'App\Models');
+        return __DIR__ . '/../stubs/' . $type . '.stub';
+    }
 
-        $traitName = Str::studly($type) . Str::studly($secondModel) . ($reverse ? 'Reverse' : '') . 'Trait';
-        $stub = File::get(__DIR__ . '/stubs/' . $type . '.stub');
+    private function createModelFile($modelName, $modelNamespace)
+    {
+        $filePath = app_path("Models/{$modelName}.php");
+        if (!File::exists($filePath)) {
+            $modelTemplate = "<?php\n\nnamespace {$modelNamespace};\n\nuse Illuminate\Database\Eloquent\Model;\n\nclass {$modelName} extends Model\n{\n    //\n}\n";
+            File::put($filePath, $modelTemplate);
+            $this->info("Model created at: {$filePath}");
+        }
+    }
 
+    private function appendRelation($type, $modelName1, $modelName2, $modelNamespace, $stub)
+    {
         $replacements = [
-            '{{ traitNamespace }}' => $traitNamespace,
-            '{{ modelNamespace }}' => $modelNamespace,
-            '{{ traitName }}' => $traitName,
-            '{{ firstModel }}' => $firstModel,
-            '{{ secondModel }}' => $secondModel,
+            '{{ modelName1 }}' => $modelName1,
+            '{{ modelName2 }}' => $modelName2,
+            '{{ modelName1Plural }}' => Str::plural($modelName1),
+            '{{ modelName2Plural }}' => Str::plural($modelName2),
+            '{{ relationName }}' => Str::camel(Str::plural($modelName2)),
+            '{{ reverseRelationName }}' => Str::camel($modelName1),
+            '{{ reverseModelName }}' => $modelName1,
+            '{{ namespace }}' => $modelNamespace,
+            '{{ modelNamespace }}' => $modelNamespace
         ];
 
         $content = str_replace(array_keys($replacements), array_values($replacements), $stub);
-
-        $fileName = $traitName . '.php';
-        $filePath = base_path('app/Traits') . '/' . $fileName;
-        $file = new SplFileInfo($filePath);
-        if (!$file->isFile()) {
-            File::put($filePath, $content);
-            $this->info($fileName . ' created successfully!');
-        } else {
-            $this->error($fileName . ' already exists!');
-        }
+        $filePath = app_path("Models/{$modelName1}.php");
+        File::append($filePath, "\n\n" . $content);
     }
 }
